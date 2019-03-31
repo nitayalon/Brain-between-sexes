4e087e80f2c911a1277f9969cf7316a9a10ef3aa
nitayalon <nitay.alon@gmail.com>
2019-03-31 21:46:09 +0300

    Adding non parametric llrt, no success so far

--- BioBank_data_analysis/Visualization_methods/non_parametrc_llrt.R
+++ BioBank_data_analysis/Visualization_methods/non_parametrc_llrt.R
@@ -6,15 +6,15 @@ nonParametricLLRT <- function(feature_name, include_sex_densities = T)
     return(NULL)
   }
   brain_feature <- results.list[[feature_name]]
-  
   grid <- seq(-4, 4, by = 0.01)
-  men_density <- logConDens(brain_feature$feature_residuals$value[brain_feature$feature_residuals$sex == 0], xgrid = grid)
-  women_density <- logConDens(brain_feature$feature_residuals$value[brain_feature$feature_residuals$sex == 1], xgrid = grid)
+  men_density <- logConDens(brain_feature$feature_residuals$value[brain_feature$feature_residuals$sex == 0], xgrid = grid ,smoothed = T)
+  women_density <- logConDens(brain_feature$feature_residuals$value[brain_feature$feature_residuals$sex == 1], xgrid = grid ,smoothed = T)
   if(include_sex_densities)
   {
     plot(log(men_density$w / women_density$w), main = feature_name)
     plot(men_density)    
-    plot(women_density)    
+    plot(women_density)
+    return(NULL)
   }
-  plot(log(men_density$w / women_density$w), main = feature_name)
+  plot(men_density$x, log(men_density$w / women_density$w), main = feature_name)
 }
