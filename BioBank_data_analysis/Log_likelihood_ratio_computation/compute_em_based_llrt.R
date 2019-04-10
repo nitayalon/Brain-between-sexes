4a4bd33f89e9a48835ed7197cf2ce096c44baa2e
nitayalon <nitay.alon@gmail.com>
2019-04-10 13:48:53 +0300

    Adding model based log-likelihood ratio

---
+++ BioBank_data_analysis/Log_likelihood_ratio_computation/compute_em_based_llrt.R
@@ -0,0 +1,28 @@
+computeModelBasedLogLikeihoodRatio <- function(em_paramters_per_feature,
+                                               men_residuals,
+                                               women_residuals,
+                                               trim_level)
+{
+  stopifnot(names(em_paramters_per_feature) ==
+              c("p","q","mu_1","mu_2","sigma_2_men","sigma_2_women"))
+  p <- em_paramters_per_feature$p
+  q <- em_paramters_per_feature$q
+  mu_1 <- em_paramters_per_feature$mu_1
+  mu_2 <- em_paramters_per_feature$mu_2
+  sigma_2_men <- em_paramters_per_feature$sigma_2_men
+  sigma_2_women <- em_paramters_per_feature$sigma_2_women
+  men_resid_sorted <- sort(men_residuals[men_residuals > -trim_level & men_residuals < trim_level])
+  women_resid_sorted <- sort(women_residuals[women_residuals > -trim_level & women_residuals < trim_level])
+  
+  men_likelihood <- p * dnorm(men_resid_sorted, mu_1, sigma_2_men) + 
+    (1-p) * dnorm(men_resid_sorted, mu_2, sigma_2_women)
+  women_likelihood <- q * dnorm(women_resid_sorted, mu_1, sigma_2_men) + 
+    (1-q) * dnorm(women_resid_sorted, mu_2, sigma_2_women)
+  
+  llrt = log(men_likelihood/ women_likelihood)
+  
+  return(list(
+    men_likelihood = men_likelihood, 
+    women_likelihood = women_likelihood,
+    llrt = llrt))
+}
