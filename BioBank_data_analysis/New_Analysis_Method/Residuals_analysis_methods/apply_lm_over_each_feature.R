b495d144a71bbeb7016b9eeadfb115a97425a62d
Nitay Alon <nitay.alon@gmail.com>
2019-07-19 06:32:11 +0000

    Adding new corrplots

--- BioBank_data_analysis/New_Analysis_Method/Residuals_analysis_methods/apply_lm_over_each_feature.R
+++ BioBank_data_analysis/New_Analysis_Method/Residuals_analysis_methods/apply_lm_over_each_feature.R
@@ -2,13 +2,13 @@ applyLinearModelOverBrainFeature <- function(feature_name,
                                              trimming_limit = 3,
                                              subject_data)
 {
-  filtering_criteria <- !is.na(full_relevant_data[,feature_name]) & 
-    full_relevant_data[,feature_name] > 0
+  filtering_criteria <- !is.na(relevant_data[,feature_name]) & 
+    relevant_data[,feature_name] > 0
   
   feature_data <- tibble(
     eid = subject_data$eid[filtering_criteria],
-    sex = full_relevant_data$Sex[filtering_criteria],
-    value = full_relevant_data[,feature_name][filtering_criteria])
+    sex = subject_data$Sex[filtering_criteria],
+    value = relevant_data[,feature_name][filtering_criteria])
   
   total_brain_volume_data <- 
     bio.bank.data[filtering_criteria,] %>% 
