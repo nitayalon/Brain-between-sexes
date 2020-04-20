57b17e537e70e3b36865bad604dfce474cf62bb8
nitayalon <nitay.alon@gmail.com>
2020-04-20 10:51:36 +0300

    Last commit prior to correlation analysis update

--- BioBank_data_analysis/Visualization_methods/gender_mixture_model_histogram.R
+++ BioBank_data_analysis/Visualization_methods/gender_mixture_model_histogram.R
@@ -8,9 +8,13 @@ plot_mix_comps <- function(x, mu, sigma, lambda) {
 plotGenderHistogram <- function(plot_data,
                                 em_results,
                                 feature_name = NULL,
-                                p = NULL, q = NULL)
+                                p = NULL, q = NULL,
+                                two_mixtures = TRUE)
 {
   em_data <- em_results$hypothesis_results$mixture_model$m_parameters
+  myColors <- brewer.pal(5,"Set1")
+  names(myColors) <- levels(plot_data$sex)
+  colScale <- scale_colour_manual(name = "sex",values = myColors)
   
   plot_title <- sprintf("Histogram of gender data, feature %s", feature_name, p ,q)
   subtitle <- sprintf("p=%s, q=%s", p ,q)
@@ -19,27 +23,39 @@ plotGenderHistogram <- function(plot_data,
                    bins = 100, 
                    alpha=.8, 
                    position="identity") + 
+    scale_fill_manual(values = alpha(c('tomato','dodgerblue'),.1)) + 
     ggtitle(plot_title, subtitle = subtitle) 
-  # +
-  #   geom_vline(xintercept = c(em_data$mu_1, em_data$mu_2))
-   
+  if(two_mixtures)
+  {
+    feature_histogram <- feature_histogram + 
+        stat_function(geom = "line",
+                      fun = plot_mix_comps,
+                      args = list(mu = em_data$mu_1, sigma = sqrt(em_data$sigma_2_men), lambda = em_data$p),
+                      colour = "blue", lwd = 1.2) +
+        stat_function(geom = "line",
+                      fun = plot_mix_comps,
+                      args = list(mu = em_data$mu_2, sigma = sqrt(em_data$sigma_2_women) , lambda = (1 - em_data$p)),
+                      colour = "blue", lwd = 1.2,linetype = "dashed") +
+        stat_function(geom = "line",
+                      fun = plot_mix_comps,
+                      args = list(mu = em_data$mu_1, sigma = sqrt(em_data$sigma_2_men), lambda = em_data$q),
+                      colour = "red", lwd = 1.0) +
+        stat_function(geom = "line",
+                      fun = plot_mix_comps,
+                      args = list(mu = em_data$mu_2, sigma = sqrt(em_data$sigma_2_women) , lambda = (1 - em_data$q)),
+                      colour = "red", lwd = 1.0,linetype = "dashed") 
+    return(feature_histogram)
+  }
   feature_histogram <- feature_histogram + 
-      stat_function(geom = "line",
-                    fun = plot_mix_comps,
-                    args = list(mu = em_data$mu_1, sigma = sqrt(em_data$sigma_2_men), lambda = em_data$p),
-                    colour = "blue", lwd = 1.2) +
-      stat_function(geom = "line",
-                    fun = plot_mix_comps,
-                    args = list(mu = em_data$mu_2, sigma = sqrt(em_data$sigma_2_women) , lambda = (1 - em_data$p)),
-                    colour = "blue", lwd = 1.2,linetype = "dashed") +
-      stat_function(geom = "line",
-                    fun = plot_mix_comps,
-                    args = list(mu = em_data$mu_1, sigma = sqrt(em_data$sigma_2_men), lambda = em_data$q),
-                    colour = "red", lwd = 1.0) +
-      stat_function(geom = "line",
-                    fun = plot_mix_comps,
-                    args = list(mu = em_data$mu_2, sigma = sqrt(em_data$sigma_2_women) , lambda = (1 - em_data$q)),
-                    colour = "red", lwd = 1.0,linetype = "dashed") 
+    stat_function(geom = "line",
+                  fun = plot_mix_comps,
+                  args = list(mu = em_data$mu_1, sigma = sqrt(em_data$sigma_2_men), lambda = em_data$p),
+                  colour = "blue", lwd = 1.2) +
+    stat_function(geom = "line",
+                  fun = plot_mix_comps,
+                  args = list(mu = em_data$mu_2, sigma = sqrt(em_data$sigma_2_women) , lambda = (1 - em_data$q)),
+                  colour = "red", lwd = 1.0,linetype = "dashed") 
   return(feature_histogram)
+  
 }
 
