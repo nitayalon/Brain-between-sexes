fb3a48c919cf9aebec21446b4c11d2f9641dec10
nitayalon <nitay.alon@gmail.com>
2020-06-04 15:51:48 +0300

    Adding new power computation

--- data_for_paper/compute_sample_size.R
+++ data_for_paper/compute_sample_size.R
@@ -1,3 +1,7 @@
+library(mixtools)
+library(ggplot2)
+library(dplyr)
+
 nullHypothesisIsTrueLlk <- function(sample_size){
   alternative_llk <- NULL
   counter = 0
@@ -66,3 +70,48 @@ alternativeHypothesisIsTrueLlk <- function(alternative_parameters,
                                scale = alternative_llk$sigma),
               llrt = 2 * (alternative_llk$loglik - null_hypothesis_llk)))
 }
+
+n_reps <- 1000
+h1_parameters <- 
+  biobank_feature_standard_analysis[[two_mixture_model_volume_features[1]]]$hypothesis_results$mixture_model$m_parameters
+llrt_1000 <- lapply(1:n_reps, function(x){nullHypothesisIsTrueLlk(1000)})
+llrt_5000 <- lapply(1:n_reps, function(x){nullHypothesisIsTrueLlk(5000)})
+llrt_8000 <- lapply(1:n_reps, function(x){nullHypothesisIsTrueLlk(8000)})
+llrt_1000_llrt <- sapply(llrt_1000, function(x){max(0,x$llrt)})
+llrt_5000_llrt <- sapply(llrt_5000, function(x){max(0,x$llrt)})
+llrt_8000_llrt <- sapply(llrt_8000, function(x){max(0,x$llrt)})
+llrt_1000_h1 <- lapply(1:n_reps, function(x){alternativeHypothesisIsTrueLlk(h1_parameters, 1000)})
+llrt_5000_h1 <- lapply(1:n_reps, function(x){alternativeHypothesisIsTrueLlk(h1_parameters, 5000)})
+llrt_8000_h1 <- lapply(1:n_reps, function(x){alternativeHypothesisIsTrueLlk(h1_parameters, 8000)})
+
+h0_1000_samples_llrt <- data.frame(data = llrt_1000_llrt,
+                                   sample_size = rep(1000,length(llrt_1000_llrt)), 
+                                   hypothesis = rep('H0',length(llrt_1000_llrt)))
+h0_5000_samples_llrt <- data.frame(data = llrt_5000_llrt,
+                                   sample_size = rep(5000,length(llrt_5000_llrt)), 
+                                   hypothesis = rep('H0',length(llrt_5000_llrt)))
+h0_8000_samples_llrt <- data.frame(data = llrt_8000_llrt,
+                                   sample_size = rep(8000,length(llrt_8000_llrt)), 
+                                   hypothesis = rep('H0',length(llrt_8000_llrt)))
+
+h1_1000_samples_llrt <- data.frame(data = llrt_1000_h1_llrt,
+                                   sample_size = rep(1000,length(llrt_1000_h1_llrt)), 
+                                   hypothesis = rep('H1',length(llrt_1000_h1_llrt)))
+h1_5000_samples_llrt <- data.frame(data = llrt_5000_h1_llrt,
+                                   sample_size = rep(5000,length(llrt_8000_h1_llrt)), 
+                                   hypothesis = rep('H1',length(llrt_8000_h1_llrt)))
+h1_8000_samples_llrt <- data.frame(data = llrt_8000_h1_llrt,
+                                   sample_size = rep(8000,length(llrt_8000_h1_llrt)), 
+                                   hypothesis = rep('H1',length(llrt_8000_h1_llrt)))
+h0_and_h1_power_plot_data <- rbind(h0_1000_samples_llrt, h0_5000_samples_llrt, h0_8000_samples_llrt,
+                                   h1_1000_samples_llrt, h1_5000_samples_llrt, h1_8000_samples_llrt)
+
+h0_and_h1_power_plot_data$sample_size = factor(h0_and_h1_power_plot_data$sample_size)
+h0_and_h1_power_plot_data_no_zeros <- h0_and_h1_power_plot_data %>% 
+  filter(data > 0)
+ggplot(h0_and_h1_power_plot_data_no_zeros, aes(x = data, color = sample_size,linetype = hypothesis)) + 
+  stat_ecdf(geom = "step") + 
+  xlab("X") + 
+  ggtitle("Emprical CDF of log-likelihood ratio", sub = "H0 and H1") +
+  geom_vline(xintercept = quantile(h0_5000_samples_llrt$data, 0.95), linetype = 'dotted', color = 'blue')
+
