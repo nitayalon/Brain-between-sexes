9e9e14d1597aa28dab6e6b93df6eb292a719e31e
nitayalon <nitay.alon@gmail.com>
2020-06-04 14:29:07 +0300

    Adding bootstarp data

--- bootstrap_em/R/biobank_em.R
+++ bootstrap_em/R/biobank_em.R
@@ -1,76 +1,67 @@
-biobank_em <- function(feature_data
-                           ,bio_bank_data = T
+biobank_bootstrap_em <- function(feature_data
+                                 ,init_parameters = NULL      
                            ,max_iter = 3000
-                           ,init_parameters = NULL, ...)
+                           ,number_of_bootstrap_iteration = 200
+                           ,...)
 {
-  full_data <- feature_data
-  if(!setequal(names(feature_data),c("men","women")))  
-  {
-    if(bio_bank_data)
-    {
-      observations <- prepareDataBioBank(feature_data)
-    }
-    else
-    {
-      observations <- prepareData(observations)
-    }
-  }
-  
-  if(is.null(init_parameters) || !validateInitEMParameters(init_parameters))
+  observations = list(men = feature_data$value[feature_data$sex == 1],
+                     women = feature_data$value[feature_data$sex == 0]) 
+  bootstrap_results = matrix(nrow = 200, ncol = 6)
+  iteration_progress = list()
+  if(is.null(init_parameters))
   {
     p <- 0.51
     q <- 0.49
     men_bar <- mean(observations$men)
     women_bar <- mean(observations$women)
-    distributions_parameters <- computeInitParameters(p,q,men_bar,women_bar)
-    m_parameters <- list(p = p, 
-                         q = q, 
-                         mu_1 = distributions_parameters$mu_1,
-                         mu_2 = distributions_parameters$mu_2, 
-                         sigma_2_men = distributions_parameters$sigma_2,
-                         sigma_2_women = distributions_parameters$sigma_2)
+    mu_1 = ((1-q) * men_bar - (1-p) * women_bar) / (p*(1-q) - q*(1-p)) 
+    mu_2 = (q * men_bar - p * women_bar) / (q*(1-p) - p*(1-q))
+    sigma_2_men = sigma_2_women = max((mu_1 - mu_2)^2*(p+q)/2*(2-p-q)/2,2)
   }
-  sample_list <- c(1000 + 1:5,2000 + 1:5 ,3000 + 1:5)
-  llk <- NULL
-  stopping_condition <- F
-  m_samples <- c()
-  for(i in 1:max_iter)
+  else{
+    p <- init_parameters$p
+    q <- init_parameters$q
+    mu_1 = init_parameters$mu_1
+    mu_2 = init_parameters$mu_2
+    sigma_2_men = init_parameters$sigma_2_men
+    sigma_2_women = init_parameters$sigma_2_women
+  }
+  start_time = Sys.time()
+  for(i in 1:number_of_bootstrap_iteration)
   {
-    # Estep
-    e_parameters <- EStep(m_parameters,observations)
-    # Mstep
-    m_parameters <- 
-      tryCatch(
-        {
-          #This was
-          # MStep(e_parameters,observations,do_not_ignore_NA = F)
-          MStep(e_parameters,observations,do_not_ignore_NA = T)
-        },
-        error=function(cond)
-        {
-          #This was
-          # MStep(e_parameters,observations,do_not_ignore_NA = F)
-          # stopping_condition <- T
-          browser()
-        }
-        )
-    if(i %in% sample_list)
-    {
-      m_samples <- rbind(m_samples,unlist(m_parameters))
-    }
-    # llk
-    llk[i] <- computeLogLikelihoodFull(observations,m_parameters)
+    population_data = bootstrap_data_sample(observations)
+    em_results = EM(population_data$men, population_data$women, 3000, p, q, mu_1, mu_2, sigma_2_men, sigma_2_women) 
+    bootstrap_results[i,] = unlist(em_results[1:6])  
+    
+    p_progress = diff(em_results[[9]])
+    q_progress = diff(em_results[[10]])
+    mu_1_progress = diff(em_results[[11]])
+    mu_2_progress = diff(em_results[[11]])
+    
+    progress = data.frame(p_progress = p_progress,
+                          q_progress = q_progress,
+                          mu_1_progress = mu_1_progress,
+                          mu_2_progress = mu_2_progress)
+    iteration_progress[[i]] = progress
+    end_time = Sys.time()
+    if(i %% 10 == 0){
+      cat(sprintf("Progress %s percent",i / number_of_bootstrap_iteration), "\n")
+      cat(sprintf("Time took %s seconds", round(end_time - start_time), 3), "\n")
+      start_time = Sys.time()
+      }
   }
-  men_responsebilities <- tibble(eid = full_data[full_data$sex == 1,]$eid,
-                                  responsebility = e_parameters$I)
-  women_responsebilities <- tibble(eid = full_data[full_data$sex == 0,]$eid,
-                                  responsebility = e_parameters$J)
-  em_results <- list(e_parameters = e_parameters,
-                     m_parameters = m_parameters,
-                     m_samples = m_samples,
-                     all.llk = llk,
-                     llk = tail(llk,1),
-                     men_responsebilities = men_responsebilities,
-                     women_responsebilities = women_responsebilities)
+  # men_responsebilities <- tibble(eid = full_data[full_data$sex == 1,]$eid,
+  #                                 responsebility = e_parameters$I)
+  # women_responsebilities <- tibble(eid = full_data[full_data$sex == 0,]$eid,
+  #                                 responsebility = e_parameters$J)
+  # em_results <- list(e_parameters = e_parameters,
+  #                    m_parameters = m_parameters,
+  #                    m_samples = m_samples,
+  #                    all.llk = llk,
+  #                    llk = tail(llk,1),
+  #                    men_responsebilities = men_responsebilities,
+  #                    women_responsebilities = women_responsebilities)
+  em_results = list(bootstrap_results = bootstrap_results, 
+                    iteration_progress = iteration_progress)
   return(em_results)
 }
--- bootstrap_em/src/em.cpp
+++ bootstrap_em/src/em.cpp
@@ -2,26 +2,25 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-NumericVector EM(NumericVector men_data, NumericVector women_data, int number_of_iterations) {
+List EM(NumericVector men_data, NumericVector women_data, int number_of_iterations,
+        double p, double q, double mu_1, double mu_2, double sigma_2_men, double sigma_2_women) {
+  NumericVector p_path(number_of_iterations);  
+  NumericVector q_path(number_of_iterations);  
+  NumericVector mu_1_path(number_of_iterations);  
+  NumericVector mu_2_path(number_of_iterations);  
+  List results(12);
   //Init values
   int m = men_data.size(); 
   int n = women_data.size();
-  double p = 0.51;
-  double q = 0.49;
-  double men_bar = mean(men_data);
-  double women_bar = mean(women_data);
-  double mu_1 = ((1-q) * men_bar - (1-p) * women_bar) / (p*(1-q) - q*(1-p)); 
-  double mu_2 = (q * men_bar - p * women_bar) / (q*(1-p) - p*(1-q));
-  double sigma_2_men = max(NumericVector::create(std::pow(mu_1 - mu_2,2)*(p+q)/2*(2-p-q)/2,2));
-  double sigma_2_women = max(NumericVector::create(std::pow(mu_1 - mu_2,2)*(p+q)/2*(2-p-q)/2,2));
-  NumericMatrix track_parameters(number_of_iterations); 
+  NumericVector I(m);
+  NumericVector J(n);
   for(int i = 0; i < number_of_iterations; i++)
   {
     //EStep
     double sigma_men = std::sqrt(sigma_2_men);
     double sigma_women = std::sqrt(sigma_2_women);
-    NumericVector I = p * dnorm(men_data,mu_1,sigma_men) / (p * dnorm(men_data,mu_1,sigma_men) + (1-p)*dnorm(men_data,mu_2,sigma_women));
-    NumericVector J = q * dnorm(women_data,mu_1,sigma_men) / (q * dnorm(women_data,mu_1,sigma_men) + (1-q)*dnorm(women_data,mu_2,sigma_women));
+    I = p * dnorm(men_data,mu_1,sigma_men) / (p * dnorm(men_data,mu_1,sigma_men) + (1-p)*dnorm(men_data,mu_2,sigma_women));
+    J = q * dnorm(women_data,mu_1,sigma_men) / (q * dnorm(women_data,mu_1,sigma_men) + (1-q)*dnorm(women_data,mu_2,sigma_women));
     //MStep
     p = mean(I);
     q = mean(J);
@@ -29,17 +28,57 @@ NumericVector EM(NumericVector men_data, NumericVector women_data, int number_of
     double j_temp = 0;
     
     for(int j = 0; j < I.size(); j ++){
-      i_temp += I(j) * men_data(j);
+      i_temp += I[j] * men_data[j];
     }
-    for(int j = 0; j < I.size(); j ++){
-      j_temp += J(j) * women_data(j);
+    for(int j = 0; j < J.size(); j ++){
+      j_temp += J[j] * women_data[j];
     }
     
     mu_1 = (i_temp + j_temp) / (sum(I) + sum(J));
     mu_2 = (sum(men_data) + sum(women_data) - (i_temp + j_temp)) / (m + n - (sum(I) + sum(J)));
-    sigma_2_men = (I %*% (men - mu_1)^2  + J %*% (women - mu_1)^2) / (sum(I) + sum(J))
-    sigma_2_women = (1-I) %*% (men - mu_2)^2 +  (1-J) %*% (women - mu_2)^2) / (m + n - (sum(I) + sum(J))
-    )  
+    
+    double weighted_men_distance_from_mu_1 = 0;
+    double weighted_women_distance_from_mu_1 = 0;
+    double weighted_men_distance_from_mu_2 = 0;
+    double weighted_women_distance_from_mu_2 = 0;
+    
+    for(int j = 0; j < I.size(); j ++){
+      weighted_men_distance_from_mu_1 += I[j] * std::pow(men_data[j] - mu_1, 2);
+      weighted_men_distance_from_mu_2 += (1-I[j]) * std::pow(men_data[j] - mu_2, 2);
+    }
+    
+    for(int j = 0; j < J.size(); j ++){
+      weighted_women_distance_from_mu_1 += J[j] * std::pow(women_data[j] - mu_1, 2);
+      weighted_women_distance_from_mu_2 += (1-J[j]) * std::pow(women_data[j] - mu_2, 2);
+    }
+    
+    sigma_2_men = (weighted_men_distance_from_mu_1  + weighted_women_distance_from_mu_1) / (sum(I) + sum(J));
+    sigma_2_women = (weighted_men_distance_from_mu_2 +  weighted_women_distance_from_mu_2) / (m + n - (sum(I) + sum(J)));
+    // Limit the variance ratio:
+    double varince_ratio = sigma_2_men / sigma_2_women;
+    if(varince_ratio < 1/3){
+      sigma_2_women = sigma_2_men * 3;
+    }
+    if(varince_ratio > 3){
+      sigma_2_women = sigma_2_men / 3;
+    }
+    p_path(i) = p;
+    q_path(i) = q;
+    mu_1_path(i) = mu_1;
+    mu_2_path(i) = mu_2;
   }
+  results[0] = p;
+  results[1] = q;
+  results[2] = mu_1;
+  results[3] = mu_2;
+  results[4] = sigma_2_men;
+  results[5] = sigma_2_women;
+  results[6] = I;
+  results[7] = J;
+  results[8] = p_path;
+  results[9] = q_path;
+  results[10] = mu_1_path;
+  results[11] = mu_2_path;
+  return(results);
 }
 
