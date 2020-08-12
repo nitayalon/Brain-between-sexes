feebdde0e8ed0e26e2291df3d66cd83a1ca1c75b
nitayalon <nitay.alon@gmail.com>
2020-08-12 13:44:42 +0300

    Changing to equal variance assumption for the log-volume features

--- BioBank_data_analysis/Final_version_markdowns/methods/em_for_equal_proportions.R
+++ BioBank_data_analysis/Final_version_markdowns/methods/em_for_equal_proportions.R
@@ -1,10 +1,16 @@
 #' Method helper for equal proportions EM
 
-computeEqualProprtionsEM <- function(feature){
+computeEqualProprtionsEM <- function(feature, not_equal_var = T){
+  browser()	
   centers_for_em <- feature %>% 
     group_by(sex) %>% 
     select(sex, value) %>% 
     summarise(avg = mean(value), std = sd(value))
+  if(not_equal_var)
+{
   equal_proportion_em <- normalmixEM(feature$value, lambda = 0.5, mu = centers_for_em$avg, sigma = centers_for_em$std, maxit = 10000)
+}else{
+  equal_proportion_em <- normalmixEM(feature$value, lambda = 0.5, mu = centers_for_em$avg, sigma = NULL, maxit = 10000, arbvar = F)  
+}
   return(equal_proportion_em)
 }
