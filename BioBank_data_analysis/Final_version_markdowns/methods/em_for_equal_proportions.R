773bfe9898bd6c5befe1050e36d5b6212d9ced01
nitayalon <nitay.alon@gmail.com>
2020-08-12 14:31:27 +0300

    Revoking changes, branching out

--- BioBank_data_analysis/Final_version_markdowns/methods/em_for_equal_proportions.R
+++ BioBank_data_analysis/Final_version_markdowns/methods/em_for_equal_proportions.R
@@ -1,16 +1,10 @@
 #' Method helper for equal proportions EM
 
-computeEqualProprtionsEM <- function(feature, not_equal_var = T){
-  browser()	
+computeEqualProprtionsEM <- function(feature){
   centers_for_em <- feature %>% 
     group_by(sex) %>% 
     select(sex, value) %>% 
     summarise(avg = mean(value), std = sd(value))
-  if(not_equal_var)
-{
   equal_proportion_em <- normalmixEM(feature$value, lambda = 0.5, mu = centers_for_em$avg, sigma = centers_for_em$std, maxit = 10000)
-}else{
-  equal_proportion_em <- normalmixEM(feature$value, lambda = 0.5, mu = centers_for_em$avg, sigma = NULL, maxit = 10000, arbvar = F)  
-}
   return(equal_proportion_em)
 }
