5b5249ee585e1b91adda582b9129ce251719eec2
nitayalon <nitay.alon@gmail.com>
2021-03-05 16:42:55 +0200

    Adding new KS plot f orNJ

--- data_for_paper/Figures/plot_ks.R
+++ data_for_paper/Figures/plot_ks.R
@@ -1,2 +1,13 @@
-str(ks_results_non_extreme_squares)
-ggplot(ks_results_non_extreme_squares, aes(KS_men))
+windowsFonts(A = windowsFont("Arial Black"))
+plot(ks_range, non_extrem__survival, 
+     type = 'l', col = '#A9A9A9', lwd = 2.5, ylab = 'CDF', xlab = 'KS statistic', 
+     cex.lab= 1.5, cex.axis = 1.5, lty = 2,
+     family="A",
+     font=2,
+     ps = 24,
+     frame.plot = FALSE)
+lines(ks_range, extrem__survival, 
+      type = 'l', col = '#A9A9A9', lwd = 2.5, lty = 3)
+lines(seq(1/400,1,1/400) * sqrt(400), ks_distribution, type = 'l', col = 'black', lwd = 2.5)
+# lines(seq(1/289,1,1/289) * sqrt(289), ks_distribution, type = 'l', col = 'black', lwd = 2.5)
+
