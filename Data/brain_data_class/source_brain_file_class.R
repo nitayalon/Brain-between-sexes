77479469c9b7b82bd5f71499af8151735abc6f83
Nitay Alon <nitalon@cs.huji.ac.il>
2024-01-15 09:07:04 +0200

    removing data

--- Data/brain_data_class/source_brain_file_class.R
+++
@@ -1,4 +0,0 @@
-source("~/Dapha_joel/Human_brain_research/Data/brain_data_class/brain_file_class/check_brain_data_file_validity.R")
-source("~/Dapha_joel/Human_brain_research/Data/brain_data_class/brain_file_class/brain_file_class.R")
-source("~/Dapha_joel/Human_brain_research/Data/brain_data_class/brain_file_class/load_brain_dictionary.R")
-source("~/Dapha_joel/Human_brain_research/Data/brain_data_class/brain_file_class/load_brain_data_from_file.R")
